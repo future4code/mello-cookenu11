a6282722d9a3369f320abb26a1ce0527248f8303
Lourenço Passos <lo.passos93@gmail.com>
2020-08-20 20:05:35 -0300

    criar receitas e pegar receitas por id

---
+++ src/data/RecipesDatabase.ts
@@ -0,0 +1,40 @@
+import { BaseDatabase } from "./BaseDatabase";
+import {Request} from "express";
+export class RecipesDatabase extends BaseDatabase {
+  private static TABLE_NAME = "Recipes";
+
+  public async createRecipe(
+    title: string,
+    description: string,
+    creation_date: string,
+    id: string
+  ): Promise<void> {
+    try {
+      await this.getConnection()
+        .insert({
+          id,
+          title,
+          description,
+          creation_date,
+        })
+        .into(RecipesDatabase.TABLE_NAME);
+
+      console.log("Recipe creation sucesseful!");
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  public async getRecipeById(id: string): Promise<any> {
+    try {
+      const result = await this.getConnection()
+        .select("title", "description")
+        .where({ id })
+        .from(RecipesDatabase.TABLE_NAME);
+
+      return result[0];
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,9 @@ import { AddressInfo } from "net";
 import { UserDatabase } from "./data/UserDatabase";
 import { signup } from "./endpoints/signup";
 import { login } from "./endpoints/login";
+import { RecipesDatabase } from "./data/RecipesDatabase";
+import { postRecipe } from "./endpoints/postRecipe";
+import getRecipeById from "./endpoints/getRecipeById";
 
 const app = express();
 dotenv.config();
@@ -21,7 +24,22 @@ const server = app.listen(process.env.PORT || 3000, () => {
 
 app.post("/signup", signup);
 app.post("/login", login);
+app.post("/recipe/:id", postRecipe);
+app.get("/recipe/:id", getRecipeById);
+
+// const newRecipe = new RecipesDatabase().createRecipe(
+//   "Miojo",
+//   "A melhor miojo do sul do Brasil",
+//   "2020-08-20",
+//   "001"
+// );
+
+// const newRecipe = new RecipesDatabase();
 
 // (async () => {
-//   console.log(await userDB.fetchUserInfoByEmail("lou@gmail.com"));
+//   console.log(await newRecipe.getRecipeById("001"));
 // })();
+
+// async () => {
+//   console.log(newRecipe);
+// };
