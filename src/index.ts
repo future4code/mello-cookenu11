68cb59e6f85f7b37d36f5e9a5da6f1258c781dc6
Lourenço Passos <lo.passos93@gmail.com>
2020-08-20 16:15:48 -0300

    progresso

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 
 import { AddressInfo } from "net";
 import { UserDatabase } from "./data/UserDatabase";
+import { signup } from "./endpoints/signup";
+import { login } from "./endpoints/login";
 
 const app = express();
 dotenv.config();
@@ -16,3 +18,10 @@ const server = app.listen(process.env.PORT || 3000, () => {
     console.error(`Failure upon starting server.`);
   }
 });
+
+app.post("/signup", signup);
+app.post("/login", login);
+
+// (async () => {
+//   console.log(await userDB.fetchUserInfoByEmail("lou@gmail.com"));
+// })();
